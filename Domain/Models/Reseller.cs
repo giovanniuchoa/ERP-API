bdda1edb8106a59524dcb2e97a2b0e72da298166
Giovanni Lucca Reque Uchoa <giovanni.uchoa@outlook.com>
2024-04-12 20:27:27 -0300

    Create reseller model

---
+++ Domain/Models/Reseller.cs
@@ -0,0 +1,11 @@
+﻿namespace CarQuery__Test.Domain.Models
+{
+    public class Reseller
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+    }
+}
