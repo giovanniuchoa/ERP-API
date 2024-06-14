8da51dc88955fe97580f481b333d93b6d5f42941
Giovanni Lucca Reque Uchoa <giovanni.uchoa@outlook.com>
2024-06-13 23:36:02 -0300

    reverting changes

--- obj/Debug/net8.0/CarQuery--Test.AssemblyInfo.cs
+++ obj/Debug/net8.0/CarQuery--Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarQuery--Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa3c16048f4f3c85aca705d539421c87930b08f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab09d5f061c7349d8baeec0afbefa53be63d010e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarQuery--Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarQuery--Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
